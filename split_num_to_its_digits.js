ac2aace0ce039b92ca1d10f25a47e22feda3b1e1
trinitysenpai <siphesihlebomela@gmail.com>
2022-03-22 18:33:24 +0200

    💡 🎨 better comments formatting

--- split_num_to_its_digits.js
+++ split_num_to_its_digits.js
@@ -1,11 +1,11 @@
 /*
-    https://www.jschallenger.com/javascript-basics/split-number-into-digits/
+    * https://www.jschallenger.com/javascript-basics/split-number-into-digits/
     
     Write a function that takes a number (a) as argument
     Split a into its individual digits and return them in an array
     Tipp: you might want to change the type of the number for the splitting
 
-    Solution (Steps)
+    * Solution (Steps)
     1. Take number and convert to string
     2. Split the string(of numbers) to an array of strings
     3. Convert each item(string) in the 
