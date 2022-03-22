606240fab1b5821760e941d1599db91894576656
trinitysenpai <siphesihlebomela@gmail.com>
2022-03-22 19:01:40 +0200

    ✨ new solution challenge at https://www.jschallenger.com/javascript-objects/access-object-properties

---
+++ accessing_object_properties_one.js
@@ -0,0 +1,12 @@
+/*
+    * https://www.jschallenger.com/javascript-objects/access-object-properties
+
+    Write a function that takes an object with two properties as argument
+    It should return the value of the property with key country
+
+    * Solution
+    Hmmm...
+*/
+function myFunction (obj) {
+    return obj.country
+}
