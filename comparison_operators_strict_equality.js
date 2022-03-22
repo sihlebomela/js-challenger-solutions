b083c66019d660993d54e5bff3839a7eae65abc9
trinitysenpai <siphesihlebomela@gmail.com>
2022-03-22 19:00:53 +0200

    ✨ new solution
    challenge at https://www.jschallenger.com/javascript-basics/comparison-strict-equality

---
+++ comparison_operators_strict_equality.js
@@ -0,0 +1,16 @@
+/*
+    * https://www.jschallenger.com/javascript-basics/comparison-strict-equality
+    
+    Write a function that takes two values, say a and b, as arguments
+    Return true if the two values are equal and of the same type
+
+    * Solution
+    Use the strict equality operator (===)
+    
+    You can checkout MDN to learn about it : 
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Strict_equality
+*/
+
+function main(a, b) {
+    return a === b;
+}
