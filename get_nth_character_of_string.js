3185db76b8cd3665a9f5d47efbf8de2063d0e843
trinitysenpai <siphesihlebomela@gmail.com>
2022-03-23 14:24:08 +0200

    ✨ new solution challenge at https://www.jschallenger.com/javascript-basics/get-nth-character-string-javascript

---
+++ get_nth_character_of_string.js
@@ -0,0 +1,11 @@
+/*
+	* https://www.jschallenger.com/javascript-basics/get-nth-character-string-javascript
+ 
+	* Solution(Steps)
+	- use 'charAt' property to access the character from string by index 
+	- add one on the index argument to make it one-index based
+*/
+
+function main(a, n) {
+	return a.charAt(n);
+}
