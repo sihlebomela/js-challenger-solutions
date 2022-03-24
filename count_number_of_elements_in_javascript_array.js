d9cce00cef213b04dab08a05ce30d060baad7571
trinitysenpai <siphesihlebomela@gmail.com>
2022-03-24 21:18:35 +0200

    ✨ new solution

---
+++ count_number_of_elements_in_javascript_array.js
@@ -0,0 +1,9 @@
+/*
+	* https://www.jschallenger.com/javascript-arrays/count-number-elements-javascript-array
+
+	* Solution(Steps)
+	- use length array property*/
+ 
+function main(a) {
+	return a.length;
+}
