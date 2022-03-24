135c7b60eb8e94bf50a0bb041a7d9f33a3b6d5ae
trinitysenpai <siphesihlebomela@gmail.com>
2022-03-24 15:24:59 +0200

    💡 add link to challenge & update the comment

--- get_first n characters of string.js
+++ get_first n characters of string.js
@@ -1,10 +1,13 @@
 /*
-	* get first n characters of string on javascript array (will find the link)
+	* https://www.jschallenger.com/javascript-arrays/get-first-elements-array-javascript
 
 	* Task
 	- Write a function that takes an array (a) as argument
-	- Extract the first 3 elements of a
+	- Extract the first 3 elements of the array
 	- Return the resulting array
+
+	* Solution
+	Use the 'slice()' array method
 */
 
 function main(a) {
