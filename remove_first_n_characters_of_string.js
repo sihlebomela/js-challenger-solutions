7588ed2cb7b7c64850a70acf92127c99d966ffbe
trinitysenpai <siphesihlebomela@gmail.com>
2022-03-27 00:00:20 +0200

    ✨ new solution

---
+++ remove_first_n_characters_of_string.js
@@ -0,0 +1,10 @@
+/*
+	* https://www.jschallenger.com/javascript-basics/remove-first-characters-string-javascript 
+
+	* Solution(Steps)
+	- use slice string method
+*/
+ 
+function main(a) {
+	return a.slice(3);
+}
