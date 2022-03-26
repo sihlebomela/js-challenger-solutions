3e11d4287650217dabcb94f57bfd0fda733960ad
trinitysenpai <siphesihlebomela@gmail.com>
2022-03-26 23:59:59 +0200

    ✨ new solution

---
+++ check_if_a_number_is_even.js
@@ -0,0 +1,10 @@
+/*
+	* https://www.jschallenger.com/javascript-basics/check-if-number-is-even
+
+	* Solution(Steps)
+	-  use the modulus operator '%' if the remainder is 0, then it's an even number
+*/
+
+function main(a) {
+	return a % 2 === 0;
+}
